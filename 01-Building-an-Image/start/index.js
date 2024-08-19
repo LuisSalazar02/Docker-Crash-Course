8eedc3bfa64fc31432bb23dcc8d84a55aa601480
LuisSalazar02 <luissalazar2002@hotmail.com>
2024-08-19 14:19:19 -0600

    End of first module

--- 01-Building-an-Image/start/index.js
+++ 01-Building-an-Image/start/index.js
@@ -1,14 +1,13 @@
-const axios = require('axios');
-const express = require('express')
+const axios = require("axios");
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get('/', async (req, res) => {
-    let image = await axios('https://source.unsplash.com/random');
-    res.send(`<img src="${image.request.res.responseUrl}" />`)
-})
+app.get("/", async (req, res) => {
+  let image = await axios("https://picsum.photos/200/300");
+  res.send(`<img src="${image.request.res.responseUrl}" />`);
+});
 
-
-app.listen((3333), () => {
-    console.log("listening on port 3333!!!!!")
-})
+app.listen(3333, () => {
+  console.log("listening on port 3333!!!!!");
+});
